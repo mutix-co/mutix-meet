fe17729c8a7725cb9925362babad99f4a15cc2aa
yutin1987 <yuting1987@gmail.com>
2020-11-13 16:52:45 +0000

    Deploying to gh-pages from  @ 43927f4422888960f8e20e49c8a90a8fcb745630 🚀

---
+++ _next/static/KE3mpSj3zyr7Xma8yby3l/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-355cb2f57c232ac543ff.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f26d0039f18efbe52100.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
